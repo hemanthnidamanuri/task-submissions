a04a0d258e86a9a9dcfed59792d55afbeb76f974
hemanthnidamanuri <hemanthnidamanuri89@gmail.com>
2018-12-03 15:34:22 +0530

    word count of a sample text file

--- python/CSV_Programs_Files/StringCount.py
+++ python/CSV_Programs_Files/StringCount.py
@@ -1,3 +1,6 @@
+#count the number of words in a given sample file 
+#input : Text.txt a text file
+#output : word count EXAMPLE: 100
 def main():
 	stringcount = 0
 	with open("Text.txt","r") as file:
